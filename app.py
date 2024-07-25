be65d4d1dd8575d3f33e5012438b46a5f90246e6
mistykk <kalyanisharma285@gmail.com>
2024-07-25 11:49:32 +0000

    added routes and html

--- app.py
+++ app.py
@@ -1,9 +1,38 @@
-from flask import Flask
-app=Flask(__name__)
+from flask import Flask, render_template
 
-@app.route("/")
+app = Flask(__name__)
+
+Jobs = [{
+    "id": 1,
+    "title": "Data Analyst",
+    "location": "Bengluru",
+    "salary": 100000
+}, {
+    "id": 2,
+    "title": "Data Scientist",
+    "location": "Noida",
+    "salary": 100000
+}, {
+    "id": 3,
+    "title": "Software Developer",
+    "location": "Pune",
+    "salary": 100000
+}, {
+    "id": 4,
+    "title": "UI Developer",
+    "location": "Hyderabad",
+    "salary": 100000
+}]
+
+
+@app.route('/')
 def hello_world():
-  return "Hello World"
+  return render_template('home.html', jobs=Jobs)
 
-if __name__=="__main__":
+@app.route('/jobs')
+def list_jobs():
+  return Jobs
+
+
+if __name__ == "__main__":
   app.run(host='0.0.0.0', debug=True)
